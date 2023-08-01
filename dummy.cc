f2149d2dfe64cf8045c9dc75a25b2b8c4ae46772
Magdalena Schreter <magdalena.schreter@tum.de>
2023-08-01 14:44:02 +0200

    update

--- dummy.cc
+++ dummy.cc
@@ -1,10 +1,14 @@
+#include <deal.II/base/mpi.h>
 #include <iostream>
 #include <unistd.h>
 
+int main(int argc, char *argv[]) {
+  using namespace dealii;
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
 
-int main()
-{
-  std::cout << "Started" << std::endl;
-  sleep(10);
-  std::cout << "Finished" << std::endl;
+  if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0) {
+    std::cout << "Started" << std::endl;
+    sleep(10);
+    std::cout << "Finished" << std::endl;
+  }
 }
