500236ede98096af51c2bb3e8b42463035cb0a37
Peter Munch <peterrmuench@gmail.com>
2023-08-01 15:05:23 +0200

    Output nr procs

--- dummy.cc
+++ dummy.cc
@@ -7,6 +7,7 @@ int main(int argc, char *argv[]) {
   Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
 
   if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0) {
+    std::cout << Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD) << std::endl;
     std::cout << "Started" << std::endl;
     sleep(10);
     std::cout << "Finished" << std::endl;
